c1accd6f42e3153c57a9e2baf892ab886014e38d
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2025-05-23 18:00:53 +0800

    Use const generics for max node height

--- src/skiplist.rs
+++ src/skiplist.rs
@@ -1,22 +1,21 @@
 use std::sync::Arc;
 
-pub struct SkipList<K: Ord> {
+pub struct SkipList<K: Ord, const MAX_HEIGHT: usize> {
     header: Arc<Node<K>>,
 }
 
 struct Node<K: Ord> {
-    height: usize,
     key: K,
     links: Vec<Arc<Node<K>>>,
 }
 
 impl<K: Ord> Node<K> {
     fn new(key: K, height: usize) -> Self {
-        Node { key, height, links: Vec::new() }
+        Node { key, links: Vec::with_capacity(height) }
     }
 
     fn height(&self) -> usize {
-        self.height
+        self.links.len()
     }
 
     fn next(&self, level: usize) -> Option<Arc<Node<K>>> {
