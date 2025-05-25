1e7b8a707f93969bfa4939c1acc5e1fc3a8ef674
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2025-05-26 00:42:22 +0800

    wip: Search + Insert with RwLock

--- src/skiplist.rs
+++ src/skiplist.rs
@@ -1,24 +1,161 @@
-use std::sync::Arc;
+use rand_core::{RngCore, SeedableRng};
+use std::{
+    array,
+    cmp::Ordering,
+    fmt::Display,
+    sync::{Arc, Mutex, RwLock},
+};
 
-pub struct SkipList<K: Ord, const MAX_HEIGHT: usize> {
-    header: Arc<Node<K>>,
+use mt19937::{Seed, MT19937};
+
+pub struct SkipList<K: Ord, const MAX_HEIGHT: usize = 14, const SEED: u32 = 15445> {
+    header: Arc<RwLock<Node<K>>>,
+    height: usize,
+    size: usize,
+    rng: MT19937,
+}
+
+impl<K: Ord, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_HEIGHT, SEED> {
+    pub fn new() -> Self {
+        let header = Arc::new(RwLock::new(Node::new_header(MAX_HEIGHT)));
+        let rng = MT19937::new_with_slice_seed(&[SEED]);
+        SkipList {
+            header,
+            height: 1,
+            size: 0,
+            rng,
+        }
+    }
+
+    pub fn empty(&self) -> bool {
+        self.height == 1
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn insert(&mut self, key: K) -> bool {
+        let mut cur = self.header.clone();
+        let mut found = false;
+        let update: [Arc<RwLock<Node<K>>>; MAX_HEIGHT] = array::from_fn(|i| {
+            let level = MAX_HEIGHT - i;
+            while let Some(next) = cur.read().unwrap().next(level) {
+                let next_node_read_lock = next.read().unwrap();
+                let next_key = next_node_read_lock.key.as_ref();
+                match next_key {
+                    Some(k) if *k < key => cur = next,
+                    Some(k) if *k == key => {
+                        // Key already exists
+                        found = true;
+                        break;
+                    }
+                    _ => break,
+                }
+            }
+            cur.clone()
+        });
+
+        if found {
+            return false;
+        }
+
+        let new_height = self.random_height();
+        if new_height > self.height {
+            self.height = new_height;
+        }
+        let new_node = Arc::new(RwLock::new(Node::new(key, new_height)));
+
+        for i in 0..new_height {
+            let node_to_update = update[MAX_HEIGHT - i].clone();
+            let mut node_to_update_write_lock = node_to_update.write().unwrap();
+            let mut new_node_write_lock = new_node.write().unwrap();
+
+            new_node_write_lock.set_next(i, node_to_update_write_lock.next(i).unwrap());
+            node_to_update_write_lock.set_next(i, new_node.clone());
+        }
+
+        true
+    }
+
+    pub fn erase(&mut self, key: &K) -> bool {
+        false
+    }
+
+    pub fn contains(&self, key: &K) -> bool {
+        return self.find(key).is_some();
+    }
+
+    fn find(&self, key: &K) -> Option<Arc<RwLock<Node<K>>>> {
+        let mut cur = self.header.clone();
+        for level in (0..self.height).rev() {
+            while let Some(next) = cur.read().unwrap().next(level) {
+                let next_node_read_lock = next.read().unwrap();
+                let next_key = next_node_read_lock.key.as_ref();
+                match next_key {
+                    Some(k) if *k < *key => cur = next,
+                    Some(k) if *k == *key => return Some(next),
+                    _ => break,
+                }
+            }
+        }
+        None
+    }
+
+    fn random_height(&mut self) -> usize {
+        let mut height: usize = 1;
+        while height < MAX_HEIGHT && self.rng.next_u32() % 4 == 0 {
+            height += 1;
+        }
+        height
+    }
+
+    pub fn print(&self) {}
+}
+
+impl<K: Ord, const MAX_HEIGHT: usize, const SEED: u32> Display for SkipList<K, MAX_HEIGHT, SEED> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl<K: Ord, const MAX_HEIGHT: usize, const SEED: u32> Drop for SkipList<K, MAX_HEIGHT, SEED> {
+    fn drop(&mut self) {}
 }
 
 struct Node<K: Ord> {
-    key: K,
-    links: Vec<Arc<Node<K>>>,
+    key: Option<K>,
+    links: Vec<Arc<RwLock<Node<K>>>>,
 }
 
 impl<K: Ord> Node<K> {
+    fn new_header(height: usize) -> Self {
+        Node {
+            key: None,
+            links: Vec::with_capacity(height),
+        }
+    }
+
     fn new(key: K, height: usize) -> Self {
-        Node { key, links: Vec::with_capacity(height) }
+        Node {
+            key: Some(key),
+            links: Vec::with_capacity(height),
+        }
     }
 
     fn height(&self) -> usize {
         self.links.len()
     }
 
-    fn next(&self, level: usize) -> Option<Arc<Node<K>>> {
-        
+    fn next(&self, level: usize) -> Option<Arc<RwLock<Node<K>>>> {
+        self.links.get(level).cloned()
+    }
+
+    fn set_next(&mut self, level: usize, next: Arc<RwLock<Node<K>>>) {
+        assert!(level < self.links.capacity());
+
+        while level < self.links.len() {
+            self.links.push(next.clone());
+        }
     }
 }
