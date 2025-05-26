52b9ef29bf918ca8374d8080085a78c3e3e4ca11
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2025-05-27 01:07:40 +0800

    refactor: Change RNG field to Arc<RwLock<MT19937>> and improve insert/erase logic

--- src/skiplist.rs
+++ src/skiplist.rs
@@ -11,7 +11,7 @@ pub struct SkipList<K: Ord, const MAX_HEIGHT: usize = 14, const SEED: u32 = 1544
     header: Arc<RwLock<Node<K>>>,
     height: usize,
     size: usize,
-    rng: MT19937,
+    rng: Arc<RwLock<MT19937>>,
 }
 
 impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_HEIGHT, SEED> {
@@ -22,7 +22,7 @@ impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_H
             header,
             height: 1,
             size: 0,
-            rng,
+            rng: Arc::new(RwLock::new(rng)),
         }
     }
 
@@ -35,33 +35,7 @@ impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_H
     }
 
     pub fn insert(&mut self, key: K) -> bool {
-        let mut cur = self.header.clone();
-        let mut found = false;
-        let update: [Arc<RwLock<Node<K>>>; MAX_HEIGHT] = array::from_fn(|i| {
-            let level = MAX_HEIGHT - i - 1;
-            loop {
-                let next = {
-                    let cur_read_lock = cur.read().unwrap();
-                    let key = cur_read_lock.key.as_ref();
-                    match cur_read_lock.next(level) {
-                        Some(arc) => arc,
-                        None => break,
-                    }
-                };
-                let next_node_read_lock = next.read().unwrap();
-                let next_key = next_node_read_lock.key.as_ref();
-                match next_key {
-                    Some(k) if *k < key => cur = next.clone(),
-                    Some(k) if *k == key => {
-                        // Key already exists
-                        found = true;
-                        break;
-                    }
-                    _ => break,
-                }
-            }
-            cur.clone()
-        });
+        let (update, _cur, found) = self.trace(&key);
 
         if found {
             return false;
@@ -92,7 +66,14 @@ impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_H
         true
     }
 
-    pub fn erase(&mut self, key: &K) -> bool {
+    fn trace(
+        &self,
+        key: &K,
+    ) -> (
+        [Arc<RwLock<Node<K>>>; MAX_HEIGHT],
+        Arc<RwLock<Node<K>>>,
+        bool,
+    ) {
         let mut cur = self.header.clone();
         let mut found = false;
         let update: [Arc<RwLock<Node<K>>>; MAX_HEIGHT] = array::from_fn(|i| {
@@ -119,12 +100,21 @@ impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_H
             }
             cur.clone()
         });
+        return (update, cur, found);
+    }
+
+    pub fn erase(&mut self, key: &K) -> bool {
+        let (update, cur, found) = self.trace(key);
 
         if !found {
             return false;
         }
 
-        let node_to_delete = cur.read().map(|node| node.next(0)).unwrap().expect("Node to erase should be found here");
+        let node_to_delete = cur
+            .read()
+            .map(|node| node.next(0))
+            .unwrap()
+            .expect("Node to erase should be found here");
 
         for i in (0..MAX_HEIGHT).rev() {
             let node_to_update = update[MAX_HEIGHT - i - 1].clone();
@@ -164,7 +154,7 @@ impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_H
                         None => break,
                     }
                 };
-                let next_read_lock = next.read().unwrap(); 
+                let next_read_lock = next.read().unwrap();
                 match next_read_lock.key.as_ref() {
                     Some(k) if *k < *key => cur = next.clone(),
                     Some(k) if *k == *key => return Some(next.clone()),
@@ -177,16 +167,22 @@ impl<K: Ord + Debug, const MAX_HEIGHT: usize, const SEED: u32> SkipList<K, MAX_H
 
     fn random_height(&mut self) -> usize {
         let mut height: usize = 1;
-        while height < MAX_HEIGHT && self.rng.next_u32() % 4 == 0 {
+        let mut rng = self.rng.write().unwrap();
+        while height < MAX_HEIGHT && rng.next_u32() % 4 == 0 {
             height += 1;
         }
         height
     }
 }
 
-impl<K: Ord + std::fmt::Debug, const MAX_HEIGHT: usize, const SEED: u32> Display for SkipList<K, MAX_HEIGHT, SEED> {
+impl<K: Ord + std::fmt::Debug, const MAX_HEIGHT: usize, const SEED: u32> Display
+    for SkipList<K, MAX_HEIGHT, SEED>
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Err(e) = f.write_fmt(format_args!("Height: {} | Size: {}\n", self.height, self.size)) {
+        if let Err(e) = f.write_fmt(format_args!(
+            "Height: {} | Size: {}\n",
+            self.height, self.size
+        )) {
             return Err(e);
         };
         let mut cur = self.header.clone();
@@ -195,11 +191,15 @@ impl<K: Ord + std::fmt::Debug, const MAX_HEIGHT: usize, const SEED: u32> Display
             match next {
                 Some(arc) => {
                     let read_lock = arc.read().unwrap();
-                    if let Err(e) = f.write_fmt(format_args!("Key: {:?} | Height: {}\n", read_lock.key, read_lock.height())) {
+                    if let Err(e) = f.write_fmt(format_args!(
+                        "Key: {:?} | Height: {}\n",
+                        read_lock.key,
+                        read_lock.height()
+                    )) {
                         return Err(e);
                     }
                     cur = arc.clone();
-                },
+                }
                 None => break,
             }
         }
@@ -207,11 +207,13 @@ impl<K: Ord + std::fmt::Debug, const MAX_HEIGHT: usize, const SEED: u32> Display
     }
 }
 
-impl<K: Ord + std::fmt::Debug, const MAX_HEIGHT: usize, const SEED: u32> Debug for SkipList<K, MAX_HEIGHT, SEED> {
+impl<K: Ord + std::fmt::Debug, const MAX_HEIGHT: usize, const SEED: u32> Debug
+    for SkipList<K, MAX_HEIGHT, SEED>
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("{self}"))
     }
-} 
+}
 
 struct Node<K: Ord> {
     key: Option<K>,
