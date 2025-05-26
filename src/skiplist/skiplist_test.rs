93453a3108901d8033099fa9d757e62b40e4139b
Bach Le Tuan Khai <bachletuankhai@gmail.com>
2025-05-27 01:07:45 +0800

    test: Add concurrent insert test for SkipList using thread scope

--- src/skiplist/skiplist_test.rs
+++ src/skiplist/skiplist_test.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, sync::Barrier, thread};
+
 use super::*;
 
 #[test]
@@ -7,9 +9,33 @@ fn test1() {
     // println!("{list}");
     assert_eq!(list.empty(), true);
     debug_assert!(list.empty());
-    
+
     for i in 0..10 {
         list.insert(i);
     }
     debug_assert_eq!(list.size(), 10);
+}
+
+#[test]
+fn test2() {
+    let n = 50;
+    let barrier = Barrier::new(n);
+
+    thread::scope(|s| {
+        let list = Arc::new(RwLock::new(SkipList::<usize>::new()));
+        let barrier = &barrier;
+        for i in 0..n {
+            let list = list.clone();
+            s.spawn(move || {
+                barrier.wait();
+                let res = list
+                    .write()
+                    .map(|mut l| l.insert(i.clone()))
+                    .expect("Should insert");
+                assert!(res);
+
+                assert!(list.read().map(|l| l.contains(&i)).unwrap());
+            });
+        }
+    })
 }
